17c3cc93a277be4de3da3d4259540c4c2d970869
Edmund Xin <mxinburritos@gmail.com>
2020-06-22 22:32:55 -0700

    Updates

---
+++ precache-manifest.dce9a2d25f1ec91d1fde373da9f0fee1.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80eef9d23fc1871446b5e843ca8964ed",
+    "url": "/weatherboy/index.html"
+  },
+  {
+    "revision": "eabdec5ce084833d8fa3",
+    "url": "/weatherboy/static/css/2.c9ad3d3e.chunk.css"
+  },
+  {
+    "revision": "48494541e420e3071bf7",
+    "url": "/weatherboy/static/css/main.dca5af65.chunk.css"
+  },
+  {
+    "revision": "eabdec5ce084833d8fa3",
+    "url": "/weatherboy/static/js/2.b654f119.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/weatherboy/static/js/2.b654f119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48494541e420e3071bf7",
+    "url": "/weatherboy/static/js/main.bb5e0a1a.chunk.js"
+  },
+  {
+    "revision": "df56f6262ccc921095a4",
+    "url": "/weatherboy/static/js/runtime-main.8ccf932b.js"
+  },
+  {
+    "revision": "a529f16b5d9226f301d70c4787a75625",
+    "url": "/weatherboy/static/media/Tokyo.a529f16b.jpg"
+  },
+  {
+    "revision": "4dfdd1c035c641accc4f3fedb7b73055",
+    "url": "/weatherboy/static/media/lato-latin-100.4dfdd1c0.woff"
+  },
+  {
+    "revision": "9ca14b8703044ec44627982d6d0e1e44",
+    "url": "/weatherboy/static/media/lato-latin-100.9ca14b87.woff2"
+  },
+  {
+    "revision": "83a45512c894889710e9966b9edddc6d",
+    "url": "/weatherboy/static/media/lato-latin-100italic.83a45512.woff"
+  },
+  {
+    "revision": "de3428c265cecf1856805f21f274bb82",
+    "url": "/weatherboy/static/media/lato-latin-100italic.de3428c2.woff2"
+  },
+  {
+    "revision": "656963c4beb814e754e38478fcaa0439",
+    "url": "/weatherboy/static/media/lato-latin-300.656963c4.woff"
+  },
+  {
+    "revision": "98d8cf792834c0bef59c2be99dc3533d",
+    "url": "/weatherboy/static/media/lato-latin-300.98d8cf79.woff2"
+  },
+  {
+    "revision": "8e90b967ea69fc68b130d36cc34d41c0",
+    "url": "/weatherboy/static/media/lato-latin-300italic.8e90b967.woff"
+  },
+  {
+    "revision": "a21767e20d27a9c06007c981a8e5f827",
+    "url": "/weatherboy/static/media/lato-latin-300italic.a21767e2.woff2"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/weatherboy/static/media/lato-latin-400.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/weatherboy/static/media/lato-latin-400.b8ee546a.woff"
+  },
+  {
+    "revision": "117e995c97eab30fb92843616018d1f1",
+    "url": "/weatherboy/static/media/lato-latin-400italic.117e995c.woff2"
+  },
+  {
+    "revision": "e652470b9572b86907b77a97f40dc915",
+    "url": "/weatherboy/static/media/lato-latin-400italic.e652470b.woff"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/weatherboy/static/media/lato-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/weatherboy/static/media/lato-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "5ce0990b59e1ed40e05327f625a24c4a",
+    "url": "/weatherboy/static/media/lato-latin-700italic.5ce0990b.woff2"
+  },
+  {
+    "revision": "9e9a574493d4cd2fa62380f376461941",
+    "url": "/weatherboy/static/media/lato-latin-700italic.9e9a5744.woff"
+  },
+  {
+    "revision": "947e87c53b5765bfc8982613ccd789e9",
+    "url": "/weatherboy/static/media/lato-latin-900.947e87c5.woff2"
+  },
+  {
+    "revision": "a82ff6ac9208656f9a21a65dfacbcae3",
+    "url": "/weatherboy/static/media/lato-latin-900.a82ff6ac.woff"
+  },
+  {
+    "revision": "73d7130e1dc196d25bb35faf2afbbb43",
+    "url": "/weatherboy/static/media/lato-latin-900italic.73d7130e.woff"
+  },
+  {
+    "revision": "bd86aaa72d23c91de85390a93aa6bef2",
+    "url": "/weatherboy/static/media/lato-latin-900italic.bd86aaa7.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/weatherboy/static/media/roboto-latin-100-italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/weatherboy/static/media/roboto-latin-100-italic.f9e8e590.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/weatherboy/static/media/roboto-latin-100-normal.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/weatherboy/static/media/roboto-latin-100-normal.7370c367.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/weatherboy/static/media/roboto-latin-300-italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/weatherboy/static/media/roboto-latin-300-italic.4df32891.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/weatherboy/static/media/roboto-latin-300-normal.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/weatherboy/static/media/roboto-latin-300-normal.ef7c6637.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/weatherboy/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/weatherboy/static/media/roboto-latin-400-italic.fe65b833.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/weatherboy/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/weatherboy/static/media/roboto-latin-400-normal.60fa3c06.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/weatherboy/static/media/roboto-latin-500-italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/weatherboy/static/media/roboto-latin-500-italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/weatherboy/static/media/roboto-latin-500-normal.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/weatherboy/static/media/roboto-latin-500-normal.87284894.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/weatherboy/static/media/roboto-latin-700-italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/weatherboy/static/media/roboto-latin-700-italic.da0e7178.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/weatherboy/static/media/roboto-latin-700-normal.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/weatherboy/static/media/roboto-latin-700-normal.adcde98f.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/weatherboy/static/media/roboto-latin-900-italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/weatherboy/static/media/roboto-latin-900-italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/weatherboy/static/media/roboto-latin-900-normal.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/weatherboy/static/media/roboto-latin-900-normal.bb1e4dc6.woff"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherboy/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherboy/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherboy/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherboy/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weatherboy/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
